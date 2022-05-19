68898ffe1b313b095ce6a79ab1e4212cb871ab6f
Hugo Jost <hug.j@hotmail.fr>
2022-05-19 16:33:53 +0200

    folders's name update - project's structure update

---
+++ Areas/Borrower/Controllers/GeneralsViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MnsLocation5.Areas.Borrower.Controllers
+{
+    public class GeneralsViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
