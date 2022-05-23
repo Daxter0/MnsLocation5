db3657d4e4f305a990ccbf1cd1ec9c32c288ef03
Hugo Jost <hug.j@hotmail.fr>
2022-05-23 10:53:52 +0200

    Areas Structure

--- Areas/Borrower/Controllers/GeneralsViewController.cs
+++ Areas/Borrower/Controllers/GeneralsViewController.cs
@@ -1,10 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace MnsLocation5.Areas.Borrower.Controllers
 {
+
+    [Area("Borrower")]
     public class GeneralsViewController : Controller
     {
+
+        private readonly ILogger<GeneralsViewController> _logger;
+
+        public GeneralsViewController(ILogger<GeneralsViewController> logger)
+        {
+            _logger = logger;
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UserHomePage2()
         {
             return View();
         }
