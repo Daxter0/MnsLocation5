2616f8094670639817136fb0bf44dae29d580536
Hugo Jost <hug.j@hotmail.fr>
2022-06-09 14:49:23 +0200

    Create material manager

--- Areas/Admin/Controllers/MaterialsManagerController.cs
+++ Areas/Admin/Controllers/MaterialsManagerController.cs
@@ -11,6 +11,7 @@ using MnsLocation5.Data;
 using MnsLocation5.Models;
 using MnsLocation5.ViewsModel;
 
+
 namespace MnsLocation5.Areas.Admin.Controllers
 {
     [Area("Admin")]
@@ -53,15 +54,9 @@ namespace MnsLocation5.Areas.Admin.Controllers
         public IActionResult Create()
         {
             var model = new CreateMaterialViewModel();
-            model.ListType = _context.Types.Select(x => new SelectListItem() { Value = x.Id.ToString(), Text = x.Name }).ToList();
-            model.ListConditions = new List<SelectListItem>()
-            {
-                new SelectListItem { Text="Neuf", Value="Neuf"},
-                new SelectListItem { Text="Bon état", Value="Bon état"},
-                new SelectListItem { Text="Moyen état", Value="Moyen état"},
-                new SelectListItem { Text="Mauvais état", Value="Mauvais état"},
-                new SelectListItem { Text="Cassé", Value="Cassé"}
-            };
+            model = ConfigureViewModelListTypes(model);
+            model = ConfigureViewModelListConditions(model);
+            
             return View(model);
         }
 
@@ -73,27 +68,22 @@ namespace MnsLocation5.Areas.Admin.Controllers
         
         public async Task<IActionResult> Create(CreateMaterialViewModel createMaterial, IFormCollection form)
         {
+            string materialNameInput = form["MaterialName"];
             string materialQuantityInput = form["NewMaterialQuantity"];
             int materialQuantity = 1;
-            try
-            {
-                materialQuantity = int.Parse(materialQuantityInput);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            if (ModelState.IsValid) //mettre un point d'arret ici afin de voir ce que recup material
+            bool parse = int.TryParse(materialQuantityInput, out materialQuantity);
+            if (ModelState.IsValid)
             {
                 for(int i = 0 ; i < materialQuantity ; i++)
                 {
-                    Material material = new Material() { Name = createMaterial.Material.Name, TypeRefId = createMaterial.MaterialTypeID, Condition = createMaterial.Material.Condition };
+                    Material material = new Material() { Name = materialNameInput, TypeRefId = createMaterial.MaterialTypeID, Condition = createMaterial.Material.Condition };
                     _context.Add(material);
                 }
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(createMaterial);
+            
         }
       
 
@@ -180,6 +170,37 @@ namespace MnsLocation5.Areas.Admin.Controllers
         private bool MaterialExists(int id)
         {
             return _context.Materials.Any(e => e.Id == id);
+        }
+
+        /// <summary>
+        /// Set up the view model listTypes to be rendered properly by the create view
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        private CreateMaterialViewModel ConfigureViewModelListTypes(CreateMaterialViewModel model)
+        {
+            model.ListType = _context.Types.Select(x => new SelectListItem() { Value = x.Id.ToString(), Text = x.Name }).ToList();
+
+            return model;
+        }
+
+        /// <summary>
+        /// Set up the view model listConditions to be rendered properly by the create view
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        private CreateMaterialViewModel ConfigureViewModelListConditions(CreateMaterialViewModel model)
+        {
+            model.ListConditions = new List<SelectListItem>()
+            {
+                new SelectListItem { Text="Neuf", Value="Neuf"},
+                new SelectListItem { Text="Bon état", Value="Bon état"},
+                new SelectListItem { Text="Moyen état", Value="Moyen état"},
+                new SelectListItem { Text="Mauvais état", Value="Mauvais état"},
+                new SelectListItem { Text="Cassé", Value="Cassé"}
+            };
+
+            return model;
         }
     }
 }
