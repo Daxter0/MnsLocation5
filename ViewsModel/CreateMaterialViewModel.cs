8e978a693d6be4b18fda5f54f59a3fbfd858afed
Hugo Jost <hug.j@hotmail.fr>
2022-06-10 16:23:09 +0200

    ViewModel :S

--- ViewsModel/CreateMaterialViewModel.cs
+++ ViewsModel/CreateMaterialViewModel.cs
@@ -15,6 +15,12 @@ namespace MnsLocation5.ViewsModel
         public List<SelectListItem> ListConditions { get; set; }
         public List<SelectListItem> ListStatut { get; set; }
         public List<List<Material>> ListOfListMaterials{ get; set; }
+        public User User { get; set; } = new User();
+        public List<User> Users { get; set; } = new List<User>();
+        public List<RentalCart> RentalCarts { get; set; } = new List<RentalCart>();
+        public RentalCart RentalCart { get; set; } = new RentalCart();
+        public MaterialRentalCart MaterialRentalCart { get; set; } = new MaterialRentalCart();
+        public List<Material> ChoosenMaterials { get; set; } = new List<Material>();
 
         public CreateMaterialViewModel()
         {
